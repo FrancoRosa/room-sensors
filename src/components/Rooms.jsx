e574a280199585206cd47b81fea55c28267abc73
FrancoRosa <km115.franco@gmail.com>
2021-08-13 13:23:43 -0500

    Add links to cards

--- src/components/Rooms.jsx
+++ src/components/Rooms.jsx
@@ -1,4 +1,5 @@
 import { useStoreState } from "easy-peasy"
+import { Link } from "react-router-dom"
 const Rooms = () => {
   const rooms = useStoreState(state => state.rooms)
 
@@ -7,12 +8,14 @@ const Rooms = () => {
       <div class="is-flex is-flex-wrap-wrap">
         {rooms.map(room => (
           <div className="card m-4">
-            <div className="card-content">
-              <p className="title is-3">{room.name} <span className='subtitle is-4'>({room.id})</span></p>
-              <p className="subtitle is-5">{room.description}</p>
-              <p>Sensors connected: {room.sensors}</p>
-              <p>Last update: {room.last_update}</p>
-            </div>
+            <Link to={`/room/${room.id}`}>
+              <div className="card-content">
+                <p className="title is-3">{room.name} <span className='subtitle is-4'>({room.id})</span></p>
+                <p className="subtitle is-5">{room.description}</p>
+                <p>Sensors connected: {room.sensors}</p>
+                <p>Last update: {room.last_update}</p>
+              </div>
+            </Link>
           </div>
         ))}
       </div>
